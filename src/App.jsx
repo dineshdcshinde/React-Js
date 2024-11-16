156e6459dcfd28417a7de86da30382364a0a3313
Dinesh_DC <dineshshinde81563@gmail.com>
2024-11-16 23:55:41 +0530

    Cleaned the boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const App = () => {
+  return (
+    <Fragment>
+      <form action="#">
+      <input type="text" name="Full Name" id="" placeholder="Enter your full name" />
+      </form>
+    </Fragment>
+  );
+};
+
+export default App;
